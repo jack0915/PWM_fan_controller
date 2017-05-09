b9ecf7c4822e2741416bef57f39bd5405b189e06
Jack Liang <24751017@qq.com>
2017-05-09 17:08:08 +0800

    Update adc.h

--- HARDWARE/adc.h
+++ HARDWARE/adc.h
@@ -5,7 +5,9 @@
 
 
 void Adc_Init(void);
-u16  Get_Adc(u8 ch); 
+u16 Get_Adc(u8 ch); 
 u16 Get_Adc_Average(u8 ch,u8 times); 
+u8 Fine_data_position(u16 *a,u8 ArrayLong,u16 data);
+u8 Get_RTC_Temperature(uc temp_A, u16 ArrayPosition);
  
 #endif 
